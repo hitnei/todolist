d9c1dd80553cd6d83afe5d23ae93ac795baef4fe
hitnei <hoangnhi050298@gmail.com>
2020-03-12 15:58:30 +0700

    fix call api

--- view/src/Config.js
+++ view/src/Config.js
@@ -5,8 +5,8 @@ export const API_URL = 'http://localhost:5000';
 var getToken = () => {
     let token = document.cookie.split(";").find(x => x.includes("authorization"));
     if (token && token.split("=")[1]) {
-      token = token.split("=")[1]
-      return token
+        token = token.split("=")[1]
+        return token
     }
     return ""
 }
@@ -17,9 +17,11 @@ export const CALLAPI = (method = 'post', path, data = {}, isHeader = true) => {
         method: method,
         url: `${API_URL}/${path}`,
         data: data,
-        headers: isHeader? { Authorization: `bearer ${token}` } : {}
-    }).then(data => {
-        return data
+        headers: isHeader ? { Authorization: `bearer ${token}` } : {}
     })
+        .then(data => {
+            return data
+        })
+        .catch(err => console.log(err))
 
 };
