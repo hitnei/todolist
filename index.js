28045d7371fdca73743654bbd759f23ea479e028
hitnei <hoangnhi050298@gmail.com>
2020-03-25 12:13:50 +0700

    fix

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ memo = require('./routes/memo');
 app.use(cors())
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('view', path.join(__dirname, 'view'));
 app.set('view engine', 'html')
 
 // parse application/x-www-form-urlencoded
@@ -54,6 +54,8 @@ app.get('/a', (req, res) => {
 
 app.post('/checkToken', verifyToken.checkToken)
 
-app.listen(5000, () => {
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
    console.log('App listening on port 5000')
 })
