a10e5667de3fccd521cba62b03b0592726bf25f8
hitnei <hoangnhi050298@gmail.com>
2020-04-08 20:52:57 +0700

    sort catefory

--- view/src/reducers/allCategory.js
+++ view/src/reducers/allCategory.js
@@ -1,11 +1,20 @@
 import * as types from './../constants/ActionTypes';
 
+var sortList = (list) => {
+    list = list.sort((item1, item2) => {
+        var name1 = item1.categoryName.toLowerCase()
+        var name2 = item2.categoryName.toLowerCase()
+        return name1 < name2 ? -1 : name1 > name2? 1 : 0
+    })
+    return list
+}
+
 var InitialState = []
 const allCategory = (state = InitialState, action) => {
     switch (action.type) {
         case types.CHANGE_ALLCATEGORY:
             state = action.data;
-            return [...state];
+            return sortList([...state]);
         case types.CHANGE_OR_ADD_CATEGORY:
             var { category } = action
             var { _id } = category
@@ -21,9 +30,9 @@ const allCategory = (state = InitialState, action) => {
                 // increase categoryAmount
                 state[index].categoryAmount = ++state[index].categoryAmount
             }
-            return [...state];
+            return sortList([...state]);
         default:
-            return state;
+            return sortList(state);
     }
 };
 export default allCategory;
